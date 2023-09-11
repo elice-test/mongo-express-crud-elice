237160e9d5f42c4f46bf73b726a5ce5bdd1b6efe
DaeGul2 <alsxogml1234@gmail.com>
2023-09-12 00:02:18 +0900

    ㅇ
    ㅇ

---
+++ server/controllers/userController.js
@@ -0,0 +1,55 @@
+const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
+
+exports.createUser = async (req, res) => {
+    try {
+        const { email, password, name } = req.body;
+        const hash = await bcrypt.hash(password, 12);//비밀번호로 해시, work factor
+        console.log("hash함수를 통과한 비밀번호 : ",hash);
+        const user = new User({
+            email,//유저네임은 그대로
+            password: hash, //비밀번호는 해시된거로
+            name
+        });
+        await user.save();//데이터베이스에 저장 (이전에 게시물 등록하던거랑 같음)
+        res.status(201).json(user);
+    } catch (e) {
+
+        res.status(500).json(e);
+
+    }
+}
+
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // 데이터베이스에서 해당 이메일을 가진 사용자를 찾습니다.
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            // 사용자가 존재하지 않을 때 오류 메시지를 반환합니다.
+            return res.status(401).json({ message: '해당 이메일로 등록된 사용자가 없습니다.' });
+        }
+
+        // 사용자의 비밀번호와 입력된 비밀번호를 비교합니다.
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        
+        
+        if (isPasswordValid) {
+            // 비밀번호가 일치할 때 사용자 정보를 반환합니다.
+            
+            console.log('일치');
+            return res.json(user);
+        } else {
+            // 비밀번호가 일치하지 않을 때 오류 메시지를 반환합니다.
+            console.log('불일치');
+            return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
+        }
+    } catch (error) {
+        // 오류가 발생한 경우 500 상태 코드와 오류 메시지를 반환합니다.
+        console.log(error);
+        return res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDb = require('./config/db');
 const postRouter = require('./routes/postRouter');
+const userRouter = require('./routes/userRouter');
 const cors = require('cors');
 
 connectDb();
@@ -12,6 +13,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api/posts',postRouter);
+app.use('/api/users',userRouter);
 
 app.listen(port, () => {
   console.log(`Server connected on port ${port}`);
